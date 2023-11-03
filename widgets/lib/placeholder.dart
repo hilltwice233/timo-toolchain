155325aaa3b285fea6424be2b35ebe31c84dd53a
hilltwice <hilltwice@163.com>
2023-11-03 16:19:22 +0800

    root wrapper

--- widgets/lib/placeholder.dart
+++ widgets/lib/placeholder.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
 
 class CenterText extends StatelessWidget {
   /// Display the given text at the center of the widget.
@@ -7,4 +7,26 @@ class CenterText extends StatelessWidget {
   final String text;
   @override
   Widget build(BuildContext context) => Center(child: Text(text));
+}
+
+class RootWrapper extends StatelessWidget {
+  /// Provide the [MediaQuery] and [TextDirection] at the root of an app.
+  /// It will enable widgets such as [Text] without a [MaterialApp].
+  ///
+  /// This widget is not modifiable and designed for debug propose.
+  /// Please write your own manager of [MediaQuery] and [TextDirection]
+  /// if you'd like to have better control over your own application.
+  const RootWrapper({super.key, this.child = const CenterText('app root')});
+  final Widget child;
+
+  @override
+  Widget build(BuildContext context) {
+    return MediaQuery(
+      data: const MediaQueryData(),
+      child: Directionality(
+        textDirection: TextDirection.ltr,
+        child: child,
+      ),
+    );
+  }
 }
