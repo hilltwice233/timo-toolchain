bcab1ff0cbb7741ff73ae770d3db85a5dc5491f1
hilltwice <hilltwice@163.com>
2023-11-07 06:44:22 +0800

    basic builder frame

--- gen/example/lib/state_data_example.dart
+++ gen/example/lib/state_data_example.dart
@@ -1,3 +1,8 @@
+import 'package:timo_anno/annotations.dart';
+
+part 'state_data_example.data.g.dart';
+
+@StateData()
 class ExampleThemeData {
   const ExampleThemeData();
 }
--- gen/lib/builders.dart
+++ gen/lib/builders.dart
@@ -1,1 +1,10 @@
+import 'package:build/build.dart';
+import 'package:source_gen/source_gen.dart';
+import 'package:timo_gen/src/state_data.dart';
 
+Builder stateDataBuilder(BuilderOptions options) {
+  return LibraryBuilder(
+    StateDataGenerator(),
+    generatedExtension: '.data.g.dart',
+  );
+}
